5ec6c5d24ad3027d13ba9adf83e4c028c53c8732
Leonel776 <leonel.mvigil@proton.me>
2026-05-18 00:26:13 -0600

    Update app_routes.dart
    Agregando página facturación a routes

--- frontend/lib/routes/app_routes.dart
+++ frontend/lib/routes/app_routes.dart
@@ -7,6 +7,7 @@ import '../screens/home_screen.dart';
 import '../screens/admin_register.dart';
 import '../pages/perfil.dart';
 import '../pages/ofertas.dart';
+import '../pages/factura.dart';
 
 class AppRoutes {
   static const String clientes = '/clientes';
@@ -14,14 +15,16 @@ class AppRoutes {
   static const String home = '/dashboard';
   static const String adminRegister = '/registroAdmin';
   static const String perfil = '/perfil';
-  static const String ofertas = '/ofertas';  
+  static const String ofertas = '/ofertas';
+  static const String factura = '/factura';
 
   static Map<String, WidgetBuilder> get routes => {
-        adminRegister: (context) => const RegistroAdmin(),
-        login: (context) => const LoginPage(),
-        clientes: (context) => const ClientesPage(),
-        home: (context) => const HomeScreen(),
-        perfil: (context) => const PerfilPage(),
-        ofertas: (context) => const OfertasScreen(),   //
-      };
+    adminRegister: (context) => const RegistroAdmin(),
+    login: (context) => const LoginPage(),
+    clientes: (context) => const ClientesPage(),
+    home: (context) => const HomeScreen(),
+    perfil: (context) => const PerfilPage(),
+    ofertas: (context) => const OfertasScreen(), //
+    factura: (context) => const FacturacionScreen(),
+  };
 }
